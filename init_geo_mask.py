d977c3bd2e392b19507a1827b6549c7402d91be1
hyeokjujeong <hyeokju0502@gmail.com>
2025-05-26 15:47:07 +0900

    5th

--- init_geo_mask.py
+++ init_geo_mask.py
@@ -13,6 +13,10 @@ from icecream import ic
 ic(torch.cuda.is_available())  # Check if CUDA is available
 ic(torch.cuda.device_count())
 
+
+from utils.sfm_utils import save_intrinsics, save_extrinsic, save_points3D, save_time, save_images_and_masks, init_filestructure, get_sorted_image_files, split_train_test, load_images, compute_co_vis_masks, load_images_single_channel
+from utils.camera_utils import generate_interpolated_path
+from utils.mask_utils import get_corresponding_mask_paths, get_object_masks
 from mast3r.model import AsymmetricMASt3R
 from mast3r.fast_nn import fast_reciprocal_NNs
 from dust3r.image_pairs import make_pairs
@@ -20,9 +24,7 @@ from dust3r.inference import inference
 from dust3r.utils.device import to_numpy
 from dust3r.utils.geometry import inv
 from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
-from utils.sfm_utils import save_intrinsics, save_extrinsic, save_points3D, save_time, save_images_and_masks, init_filestructure, get_sorted_image_files, split_train_test, load_images, compute_co_vis_masks, load_images_single_channel
-from utils.camera_utils import generate_interpolated_path
-from utils.mask_utils import get_corresponding_mask_paths, get_object_masks
+
 
 
 def main(source_path, model_path, ckpt_path, device, batch_size, image_size, schedule, lr, niter, 
